975c871c6bb9da58b1985dcfd71401c212dd5e4f
Ali KARABAY <alikarabay77@gmail.com>
2023-05-13 03:13:51 +0300

    auth added.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,22 @@
 import Header from "@/components/Header";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 // @ts-ignore
 export default function Layout({children}) {
+	const router = useRouter();
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const token = localStorage.getItem('token');
+			if (!token) {
+				router.push('/auth');
+			} else {
+				router.push('/');
+			}
+		}
+	}, []);
+
 	return (
 		<div className="container mx-auto">
 			<Header/>
