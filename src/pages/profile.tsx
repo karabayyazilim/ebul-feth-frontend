9ff0cf4c1fd5b2f9489afd278c3cb56f67d13ad6
Trou95 <gorkem_095@hotmail.com>
2023-07-09 13:02:47 +0300

    Profil Düzenleme Eklendi

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "@/lib/axios";
+import toast from "react-hot-toast";
 import DashboardLayout from "@/layouts/DashboardLayout";
 
 Profile.getLayout = (page: any) => <DashboardLayout>{page}</DashboardLayout>;
 
 interface Profile {
+  login: string;
   full_name: string;
   email: string;
   avatar: string;
@@ -12,16 +14,37 @@ interface Profile {
 
 export default function Profile() {
   const [profile, setProfile] = useState<Profile>({
+    login: "",
     avatar: "",
     email: "",
     full_name: "",
   });
+
+  const [login, setLogin] = useState("");
+  const [file, setFile] = useState<File>();
+
+  const onFileChange = (e: any) => {
+    setFile(e.target.files[0]);
+  }
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+
+    var bodyFormData = new FormData();
+    bodyFormData.append("login", login);
+    bodyFormData.append("avatar", file ?? "");
+    axios.put("/user/update-profile", bodyFormData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      }}).then((res) => toast.success("Success")).catch(e => toast.error(e.message));
+  }
 
   useEffect(() => {
     axios
       .get("/auth/my-account")
       .then((res) => {
         setProfile(res.data);
+        setLogin(res.data.login);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -36,39 +59,36 @@ export default function Profile() {
           </div>
           <div className="flex-1">
             <h1 className="text-4xl font-bold">{profile.full_name}</h1>
-            <p className="text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              euismod
-            </p>
           </div>
         </div>
         <div className="card">
           <div className="card-body">
             <h2 className="card-title">Profile</h2>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="John Doe"
-                className="input input-bordered"
-              />
-
-              <label className="label">
-                <span className="label-text">Bio</span>
-              </label>
-              <textarea className="textarea h-24 textarea-bordered"></textarea>
-
-              <label className="label">
-                <span className="label-text">Avatar</span>
-              </label>
-              <input
-                type="file"
-                className="file-input file-input-bordered w-full"
-              />
-            </div>
-            <button className="btn btn-outline btn-success mt-6">Save</button>
+            <form onSubmit={onSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                    type="text"
+                    name="login"
+                    value={login}
+                    onChange={e=> setLogin(e.target.value)}
+                    className="input input-bordered"
+                />
+                <label className="label">
+                  <span className="label-text">Avatar</span>
+                </label>
+                <input
+                    type="file"
+                    name="file"
+                    onChange={onFileChange}
+                    className="file-input file-input-bordered w-full"
+                />
+              </div>
+              <button
+                  className="btn btn-outline btn-success mt-6">Save</button>
+            </form>
           </div>
         </div>
       </div>
